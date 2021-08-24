1fb80b26f310d8633a9edfdffb54a7dd9de4b2ca
unknown <julisdru@gmail.com>
2021-08-24 16:50:21 -0300

    pagina principa, inciar sesion, prueba de una tabla

---
+++ frontend/src/components/InfoSection/Data.js
@@ -0,0 +1,55 @@
+
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Qué es',
+    headLine: 'textoooooooooo',
+    description: 'textoooooooo',
+    buttonLabel: 'Iniciar Sesión',
+    imgStart: false,
+    img: require('../../images/img.svg'),
+    alt: 'ver',
+    dark: true,
+    primary: true,
+    darkText: false
+}
+
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Qué es',
+    headLine: 'textoooooooooo',
+    description: 'textoooooooo',
+    buttonLabel: 'Iniciar Sesión',
+    imgStart: true,
+    img: require('../../images/img.svg'),
+    alt: 'ver',
+    dark: false,
+    primary: false,
+    darkText: true
+}
+
+
+export const homeObjThree= {
+    id: 'signup',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Qué es',
+    headLine: 'textoooooooooo',
+    description: 'textoooooooo',
+    buttonLabel: 'Iniciar Sesión',
+    imgStart: false,
+    img: require('../../images/img.svg'),
+    alt: 'ver',
+    dark: false,
+    primary: false,
+    darkText: true
+
+}
+
